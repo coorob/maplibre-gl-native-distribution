769685101f607d8786bd050905043eb0c2a9b128
Robert Cook <robertcook.rdc@gmail.com>
2026-06-01 21:10:11 +0200

    MapLibre 6.26.0-traska.4 terrain streaming distribution

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.26.0-traska.3/MapLibre.xcframework.zip",
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.26.0-traska.4/MapLibre.xcframework.zip",
             checksum: "127bf1cae2b2a50c2e240fb0bb329657e3a6a91968046fb5c11b8885fc8e6f8c")
     ]
 )
