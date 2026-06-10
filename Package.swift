a2cb74d05ff5765bff5c54c69ecbfe883ff0cafd
Robert Cook <robertcook.rdc@gmail.com>
2026-06-10 20:57:13 +0200

    MapLibre 6.26.0-traska.15: distance-ranked drape budgets (fork e5a62353)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.26.0-traska.14/MapLibre.xcframework.zip",
-            checksum: "6ed6305b0737de02fb9e2768cba7d3a04ba50187f622f61ffe00fdd255524f52")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.26.0-traska.15/MapLibre.xcframework.zip",
+            checksum: "72b756a1eebd64c98ab33222335e2e30a53f9f9dbae956232805f0d9c8c3ee85")
     ]
 )
