4999bd419171bb028719aaf5a921c46f350d229a
Robert Cook <robertcook.rdc@gmail.com>
2026-06-10 22:29:31 +0200

    MapLibre 6.26.0-traska.16: drape parent fallback default 1 (fork ab903177)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.26.0-traska.15/MapLibre.xcframework.zip",
-            checksum: "72b756a1eebd64c98ab33222335e2e30a53f9f9dbae956232805f0d9c8c3ee85")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.26.0-traska.16/MapLibre.xcframework.zip",
+            checksum: "a30b3f8f9abc2138e54cd9688aa47ecc5e2d27c4e1eb975c50492c5188930bd9")
     ]
 )
