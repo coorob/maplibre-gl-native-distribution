a52727847798a5bfca50e1e1b0cfd5a8f50846cd
Robert Cook <robertcook.rdc@gmail.com>
2026-07-04 10:29:30 +0200

    MapLibre 6.27.0-traska.5: zoom-transition prune hold + overscan ring 2 (fork 34181128)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.4/MapLibre.xcframework.zip",
-            checksum: "448f651994d7413759a9e4de0a67e0cca499018ec2da307b96a56dee3a479a4f")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.5/MapLibre.xcframework.zip",
+            checksum: "32843a522ff8311037ebedba332c640c1d40392ac48db08d34fb59dfbc59cdc9")
     ]
 )
