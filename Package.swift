e48b69a766eec05391f1a0bd5def1bac15e883de
Robert Cook <robertcook.rdc@gmail.com>
2026-07-04 20:36:34 +0200

    MapLibre 6.27.0-traska.18: raster sources join the 40-degree LOD gate (beige-wedge prong 3)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.17/MapLibre.xcframework.zip",
-            checksum: "d519c30e62289d16cba10191a438bbc47a67ab7e8a9b415fa2ed54442eef1676")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.18/MapLibre.xcframework.zip",
+            checksum: "e64184d3030648155a7127219b9a5c166142491fa0fab6fdb6b382f7b972c434")
     ]
 )
