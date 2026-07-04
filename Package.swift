27b145aa1c66e4a64efed95adee8c6aca562741f
Robert Cook <robertcook.rdc@gmail.com>
2026-07-04 09:16:06 +0200

    MapLibre 6.27.0-traska.4: drape overscan ring default-on + hold stale drawable through not-ready refresh (fork 477549d6)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.3/MapLibre.xcframework.zip",
-            checksum: "37d7ed7a55e5ef36867e4a7556de6a59da689eeeb7076691bd7c942681f6a80a")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.4/MapLibre.xcframework.zip",
+            checksum: "448f651994d7413759a9e4de0a67e0cca499018ec2da307b96a56dee3a479a4f")
     ]
 )
