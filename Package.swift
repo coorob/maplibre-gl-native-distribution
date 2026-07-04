e394cdf92a2fc892f9fa5a33597545ae554609f1
Robert Cook <robertcook.rdc@gmail.com>
2026-07-04 14:47:43 +0200

    MapLibre 6.27.0-traska.8: empty-DEM origin-level fix + 1 Hz cover telemetry (fork cf45a6c7)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.7/MapLibre.xcframework.zip",
-            checksum: "f1fc8e4c705c7d07dbbe38aedb90bd29bbc7cbfcfe96296add80175251e08706")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.8/MapLibre.xcframework.zip",
+            checksum: "fc8e13896335784f258830b8554a82988450093ab3eb6ccbe04287d7d274adba")
     ]
 )
