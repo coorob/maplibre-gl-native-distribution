1001278353540615b992f965f4ef33285a18b7a4
Robert Cook <robertcook.rdc@gmail.com>
2026-07-09 23:44:07 +0200

    MapLibre 6.27.0-traska.26: renderability sync hoisted out of the source loop (2D black-flash fix)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.25/MapLibre.xcframework.zip",
-            checksum: "68c1cf4a5ff8c4fa5ce808485c4ed6cc57a1ca426e98d0a995a524486dc71763")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.26/MapLibre.xcframework.zip",
+            checksum: "28cdc02eccf0bf4b009033ea81191e952e5ad846a305b4e32a8b3b4ba6ac5fd3")
     ]
 )
