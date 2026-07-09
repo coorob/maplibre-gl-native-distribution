8df90e56f0a8aac70c6fa812f14fd1adbf410ac8
Robert Cook <robertcook.rdc@gmail.com>
2026-07-09 22:50:03 +0200

    MapLibre 6.27.0-traska.21: DIAG sampled-before-rendered probes (2D black-flash hunt)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.20/MapLibre.xcframework.zip",
-            checksum: "00097b20022507ec14295ed5ab30eeccc95e934ee257a6d637fdfc248fd1cb74")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.21/MapLibre.xcframework.zip",
+            checksum: "f7015ca7b8ca4ef60a10e931ba6676509b8aa20aa530f2f3e74f8d225ada8847")
     ]
 )
