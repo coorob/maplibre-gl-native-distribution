f08dd96cedbbb812e3429e8b17e988c5288bfd3a
Robert Cook <robertcook.rdc@gmail.com>
2026-07-09 23:03:59 +0200

    MapLibre 6.27.0-traska.22: DIAG nil-bind echo + drawcall-dip detector

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.21/MapLibre.xcframework.zip",
-            checksum: "f7015ca7b8ca4ef60a10e931ba6676509b8aa20aa530f2f3e74f8d225ada8847")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.22/MapLibre.xcframework.zip",
+            checksum: "a6a1df9eea48addf94173125969ac785a4ea26e828b71fa8825657e4ae8c5a6e")
     ]
 )
