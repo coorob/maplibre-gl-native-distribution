359587b1c6eca0cfa58a99007cc4124e11927a84
Robert Cook <robertcook.rdc@gmail.com>
2026-07-09 23:36:22 +0200

    MapLibre 6.27.0-traska.25: DIAG LAYERFLAP probe

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.24/MapLibre.xcframework.zip",
-            checksum: "1f71e7990d5d0853fb7513ef916d17ad3f018159c0b7cf06b9ae8a8926385a89")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.25/MapLibre.xcframework.zip",
+            checksum: "68c1cf4a5ff8c4fa5ce808485c4ed6cc57a1ca426e98d0a995a524486dc71763")
     ]
 )
