16d700d4fab9de5b783ce81cf99eae1eb734620f
Robert Cook <robertcook.rdc@gmail.com>
2026-07-10 17:01:08 +0200

    MapLibre 6.27.0-traska.28: DIAG style-mutation + relief-lifecycle probes

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.27/MapLibre.xcframework.zip",
-            checksum: "82a9f6449312f9e368c30511240d6798849b366e8aa34325df24d31453a452e7")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.28/MapLibre.xcframework.zip",
+            checksum: "7b32103e3f2bc1b82af100f9cc84efbdbd775047eb6f019c2bd4e3fc09bc8354")
     ]
 )
