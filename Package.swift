ac9975526859361e46b2523c087146374adf1cc6
Robert Cook <robertcook.rdc@gmail.com>
2026-07-11 20:01:47 +0200

    6.27.0-traska.57: full-quad drape + painter order + safe supersede

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.56/MapLibre.xcframework.zip",
-            checksum: "f315aa12cdc090549d1cd94109dad9d85910b725eb47163d6726a18ef7bb0787")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.57/MapLibre.xcframework.zip",
+            checksum: "b5a50de00479e1fe5852fd22b4cbdf3031089d3556ba07970244c5a062b5f9fe")
     ]
 )
