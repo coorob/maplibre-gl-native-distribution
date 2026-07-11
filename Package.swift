ed2334272da53d5827c595214281291f34b65c98
Robert Cook <robertcook.rdc@gmail.com>
2026-07-12 00:24:30 +0200

    6.27.0-traska.67: eliminate overlapping terrain meshes (fork aa31738)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.66/MapLibre.xcframework.zip",
-            checksum: "04b87c867e5300fc59cf96bea405a2f3e67486c007a5fc236b0f5fa9e0214bb6")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.67/MapLibre.xcframework.zip",
+            checksum: "8e84096d9db73bcc31567a0c70805d1df0f6024db50c86808e4c48fae8024094")
     ]
 )
