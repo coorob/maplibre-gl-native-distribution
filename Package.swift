46f4c0e4686d2a0b1c38ea7e1d36fedee2c865df
Robert Cook <robertcook.rdc@gmail.com>
2026-07-11 17:49:48 +0200

    6.27.0-traska.54: DIAG — readiness-cycle tracking

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.53/MapLibre.xcframework.zip",
-            checksum: "e3e21739ff28989de802374e69629cd97a74fae684b35519d7e93130b2c249de")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.54/MapLibre.xcframework.zip",
+            checksum: "2e95b4be27ce3a080f5b0a9a1f1e3f535451cf56a530aca6f7e2bbb67cc784e4")
     ]
 )
