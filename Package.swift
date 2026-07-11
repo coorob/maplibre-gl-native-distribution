848da82088fe77d94526348f3a22f55b21478088
Robert Cook <robertcook.rdc@gmail.com>
2026-07-11 11:48:22 +0200

    6.27.0-traska.47: flyover flight fixes (cap + resize freeze + stale-bake eviction), FLYDIAG opt-in

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.46/MapLibre.xcframework.zip",
-            checksum: "47769bf1b7e1fa3f8004f51c3951bb89552d6c4d5201374c799fb65a9f5d0922")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.47/MapLibre.xcframework.zip",
+            checksum: "0db91b5721f7f1617746f60370976d2cddfe4d90140ec38e92149bfefb40642a")
     ]
 )
