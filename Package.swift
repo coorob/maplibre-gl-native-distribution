8a5eb3e57b3b4df75552007b29bc132ad62045a0
Robert Cook <robertcook.rdc@gmail.com>
2026-07-11 16:51:22 +0200

    6.27.0-traska.51: velocity-lookahead drape strip + path-segment ranking

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.50/MapLibre.xcframework.zip",
-            checksum: "167a073f0c331d8cf3b52b5937e88dfa04f9838e4508611ba446ee04c311ac30")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.51/MapLibre.xcframework.zip",
+            checksum: "1665db990c67cc1fa16a91f4807e4321f17b6cc08eca46d64a21c5d0097c99df")
     ]
 )
