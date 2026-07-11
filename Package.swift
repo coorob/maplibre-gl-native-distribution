3c7457c05b3eade4ae3dcfb77accb8a9bf907ee1
Robert Cook <robertcook.rdc@gmail.com>
2026-07-11 19:40:51 +0200

    6.27.0-traska.56: never cache failed canvas allocations; green attribution

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.55/MapLibre.xcframework.zip",
-            checksum: "ae7eb477461a9658e1124358ae62d6af6cddf8164e54593ea3445b5f19d6f6b4")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.56/MapLibre.xcframework.zip",
+            checksum: "f315aa12cdc090549d1cd94109dad9d85910b725eb47163d6726a18ef7bb0787")
     ]
 )
