5a36ff5f29df2caf749ec97c819838e7e9463393
Robert Cook <robertcook.rdc@gmail.com>
2026-07-11 10:55:47 +0200

    6.27.0-traska.46: DIAG — flyover FLYDIAG probes default-on

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.45/MapLibre.xcframework.zip",
-            checksum: "2f1d9c4aafae4a63efda41085df6fbcba8e3093ea5ab916a3c687bda962e5fe4")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.46/MapLibre.xcframework.zip",
+            checksum: "47769bf1b7e1fa3f8004f51c3951bb89552d6c4d5201374c799fb65a9f5d0922")
     ]
 )
