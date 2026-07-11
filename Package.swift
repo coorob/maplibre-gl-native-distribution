ce75c21ee550ba1ba608e0ec21d3c8602a560ef6
Robert Cook <robertcook.rdc@gmail.com>
2026-07-11 09:42:01 +0200

    6.27.0-traska.44: bake-carry + prebake gate (shading-pop polish)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.43/MapLibre.xcframework.zip",
-            checksum: "870b4df12cf631bd5f448cf7cdbf3a06cc21fad0d217d4e73f9f5db51d8857a0")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.44/MapLibre.xcframework.zip",
+            checksum: "fcf070916f3ff9fdeb41b3bddbb0311fa5287a6082e8f28f6555bd4fa336fd36")
     ]
 )
