458b724a04e41cac2adfbd90e03e9d08549be653
Robert Cook <robertcook.rdc@gmail.com>
2026-07-12 17:47:45 +0200

    6.27.0-traska.71: publish residual drape diagnostic

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.70/MapLibre.xcframework.zip",
-            checksum: "a7ae4b533cb91538696c9be292c72531adb90f387d3c33879a13eb3809ed2c46")
+            url: "https://github.com/coorob/maplibre-gl-native-distribution/releases/download/6.27.0-traska.71/MapLibre.xcframework.zip",
+            checksum: "8b8df5650a58324650f001d8ed96af34a93bfc9c9c369fb6b5a5ae441a92cbb7")
     ]
 )
